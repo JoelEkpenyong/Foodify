59673ed75e3c8bb1d3dfe6ce61b8d3c3966ac646
JoelEkpenyong <joelekpenyong@gmail.com>
2020-07-16 20:37:50 +0100

    updated

--- form.php
+++ form.php
@@ -3,10 +3,10 @@
 
     // database information
     $url = parse_url(getenv('mysql://bdbfa2b2f48496:f8434cfd@us-cdbr-east-02.cleardb.com/heroku_db670458dcc6813?reconnect=true'));
-    $server = $url["us-cdbr-east-02.cleardb.com"];
-    $username = $url["bdbfa2b2f48496"];
-    $password = $url["f8434cfd"];
-    $db = substr($url["heroku_db670458dcc6813"], 1);
+    $server = "us-cdbr-east-02.cleardb.com";
+    $username = "bdbfa2b2f48496";
+    $password = "f8434cfd";
+    $db = "heroku_db670458dcc6813";
 
 
     // connecting to the database
